5203fa9693cf005ffe08dc0f3365a68cb4bcdb5f
tpdus <tpdus6401@naver.com>
2026-08-16 17:57:09 +0900

    Chore/#1 : SAPUI5 환경 설정

---
+++ zais/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("zais.scm.zais.controller.App", {
+      onInit() {
+      }
+  });
+});
